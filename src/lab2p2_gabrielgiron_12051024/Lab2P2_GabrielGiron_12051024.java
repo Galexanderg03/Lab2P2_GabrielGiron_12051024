2f548d092104ba86d564ad5c33ad788005e7916a
Galexanderg03 <galexanderg03@gmail.com>
2022-01-28 14:11:30 -0600

    Terminando Opcion 3

--- src/lab2p2_gabrielgiron_12051024/Lab2P2_GabrielGiron_12051024.java
+++ src/lab2p2_gabrielgiron_12051024/Lab2P2_GabrielGiron_12051024.java
@@ -19,14 +19,34 @@ public class Lab2P2_GabrielGiron_12051024 {
         // TODO code application logic here
         AnimalesPorDefecto();
         int opcion = -1;
-        while(opcion != 4)
+        while(opcion != 5)
         {
             opcion = Integer.parseInt(
                     JOptionPane.showInputDialog(
                             "1) Agregar Animal\n"+
                             "2) Editar Animal\n"+
-                            "3)"
+                            "3) Imprimir Animal\n"
+                          + "4) Alimentar Animal\n"
+                          + "5)Salir"
                     ));
+            switch(opcion)
+            {
+                case 1:
+                {
+                    
+                    break;
+                }
+                case 2:
+                {
+                    
+                    break;
+                }
+                case 3:
+                {
+                    Imprimir();
+                    break;
+                }
+            }
         }
     }
     
@@ -36,7 +56,7 @@ public class Lab2P2_GabrielGiron_12051024 {
         String NombreCientifico1 = "Cervidae";
         String Nombre1 = "Ciervo";
         String Habitat1 = "Bosques";
-        String Alimentacion1 = "Plantas";
+        String Alimentacion1 = "Herbivoro";
         String Rasgos1 = "Cola Corta";
         String Geografia1 = "Europa";
         int Vida1 = 10;
@@ -56,10 +76,58 @@ public class Lab2P2_GabrielGiron_12051024 {
         String NombreCientifico3 = "Selachimorpha";
         String Nombre3 = "Tiburon";
         String Habitat3 = "Mar Abierto";
-        String Alimentacion3 = "Peces Oseos";
+        String Alimentacion3 = "Carnivoro";
         String Rasgos3 = "Colmillos Afilados";
         String Geografia3 = "SudaAfrica";
         int Vida3 = 200;
         Animales.add(new Animal(NombreCientifico3, Nombre3, Habitat3, Alimentacion3, Rasgos3, Geografia3, Vida3));
+    }
+    
+    public static void Imprimir()
+    {
+        int OP = Integer.parseInt(JOptionPane.showInputDialog(
+                          "1) Imprimir por Posicion de Lista\n"
+                        + "2) Imprimir Lista Completa\n"
+                        + "3) Imprimir por Nombre Cientifico"
+        ));
+        
+        switch(OP)
+        {
+            case 1:
+            {
+                int pos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la Posicion del Animal en la lista"));
+                if(pos < Animales.size())
+                {
+                    JOptionPane.showMessageDialog(null, Animales.get(pos).toString());
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null, "Numero Ingresado es mayor que el tamaño de la Lista");
+                }
+                break;
+            }
+            case 2:
+            {
+                String Salida = "";
+                for (int i = 0; i < Animales.size(); i++) 
+                {
+                    Salida = Salida + Animales.get(i).toString() + "\n";
+                }
+                JOptionPane.showMessageDialog(null, Salida);
+                break;
+            }
+            case 3:
+            {
+                String Nombre = JOptionPane.showInputDialog("Ingrese el nombre cientifico del animal");
+                for (int i = 0; i < Animales.size(); i++) {
+                    if(Nombre.equals(Animales.get(i).getNombreCientifico()))
+                    {
+                        JOptionPane.showMessageDialog(null, Animales.get(i).toString());
+                        break;
+                    }
+                }
+                break;
+            }
+        }
     }
 }
